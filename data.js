40aaa96b97e0642d77661cd923754c435081c7e6
Ipatkepot <ifatfatahillah3@gmail.com>
2026-03-06 18:37:20 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+// Database Siswa Angkatan
+   const students = [
+        { 
+            id: 1, 
+            name: "Fatahillah Akbar", 
+            class: "RPL 1", 
+            quote: "You make me sick , Because I adore you so", 
+            goal: "Designer ", 
+            birthDate: "18 Januari 2009",
+            favoriteMusic: "All I Want - The Panturas",
+            ig: "patkepott", 
+            github: "Ipatkepot",
+            img: "siswa/ipat.jpg" 
+        },
+        { 
+            id: 2, 
+            name: "Muhammad Reza Aditia", 
+            class: "RPL 1", 
+            quote: "4r4pSuk4m1nuM😜", 
+            goal: "Fullstack Developer", 
+            birthDate: "20 Desember 2009",
+            favoriteMusic: "-",     
+            ig: "rezraadtaa", 
+            // github: "RezaAditia",
+            img: "siswa/rejaa.jpg" 
+        },
+        { 
+            id: 3, 
+            name: "Muhammad Alham Fadhilah", 
+            class: "RPL 2", 
+            quote: "pasha lagi tidur", 
+            goal: "Jadi Rich People", 
+            birthDate: "14 Desember 2007",
+            favoriteMusic: "Don't Tell 'Em - Jeremih",     
+            ig: "allhmfdlh", 
+            github: "lilpsyaa",
+            img: "siswa/alham.jfif" 
+        },
+        { 
+            id: 4, 
+            name: "Rudy Gunawan", 
+            class: "RPL 1", 
+            quote: "akan ku beli omongan semua orang dengan hasil dan usahaku sendiri", 
+            goal: "pengusaha BADAG", 
+            birthDate: "13 Desember 2007",
+            favoriteMusic: "cindai x raja d laut",     
+            ig: "whosgun_13", 
+            // github: "lilpsyaa",
+            img: "siswa/guni.jfif" 
+        },
+        {   
+            id: 5,
+            name: "Muhammad Defahni", 
+            class: "RPL 2", 
+            quote: "Born as a human being brought up by social media", 
+            goal: "enjoy wealth", 
+            birthDate: "21 Juli 2009",
+            // favoriteMusic: "undefined",
+            ig: "___dfahniiicrtvy", 
+            github: "fahninihbous",
+            img: "siswa/aden.jfif"
+         }, 
+        {   
+            id: 6,
+            name: "Muhammad Ilham", 
+            class: "RPL 1", 
+            quote: "lebih baik gagal dari pada tidak mencoba sama sekali", 
+            goal: "freedom financial", 
+            birthDate: "26 Februari 2009",
+            favoriteMusic: "sabar sudewok",
+            ig: "mhmdhamm_26", 
+            github: "ilham269",
+            img: "siswa/ilham.jfif"
+         }, 
+         {   
+            id: 7,
+            name: "Riska Aulia", 
+            class: "RPL 1", 
+            quote: "Allah tidak pernah menaruh tanggung jawab di bahu yang salah, jika kamu terpilih, berarti kamu yang mampu 2:286", 
+            goal: "jadi bos muda yaAllah", 
+            birthDate: "16 Januari 2009",
+            favoriteMusic: "semua yang enak di denger",
+            ig: "riskauls", 
+            github: "riskaaulias",
+            img: "siswa/bais.jfif"
+         },     
+];
